076156ae10fd119f802b0916e06b62edfd3191ed
qitiandasheng <4465021+qitiandashengsunwukong@users.noreply.github.com>
2025-06-13 21:27:38 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -36,17 +36,48 @@ document.addEventListener('DOMContentLoaded', () => {
     const observer = new MutationObserver((mutationsList, observer) => {
         for (const mutation of mutationsList) {
             if (mutation.type === 'childList') {
-                const element0 = document.querySelector('#platform > div > div.platform-ui-service-header-container.mweb-titlebar-logo-container > div.header-right');
+                const element0 = document.querySelector('#app > div:nth-child(2) > header > div > div:nth-child(2) ');
                 if (element0) {
                     element0.style.display = 'none';
                 };const element1 = document.querySelector('
-#platform > div > div.container-o04Qvp.mvweb-layout-sider-container > div.side-menu-OVQnao.mweb-side-menu-container.visible-MBg7Yj > div.menuWrapper-bPZ0SU > div:nth-child(2)');
+#app > div:nth-child(2) > header > div > div.scroll-area.kling-scroll-dark > ul > a:nth-child(5) > div > div');
                 if (element1) {
                     element1.style.display = 'none';
                 };const element2 = document.querySelector('
-#Activity > div > span > div > svg');
+#app > div:nth-child(2) > header > div > div:nth-child(2) > div.menu-item.invitation-box > span > div');
                 if (element2) {
                     element2.style.display = 'none';
+                };const element3 = document.querySelector('
+#main-container > div > div.history-stream > div.filter-bar > div.right > div');
+                if (element3) {
+                    element3.style.display = 'none';
+                };const element4 = document.querySelector('
+#main-container > div > div:nth-child(1) > div > div.designer-component.property-panel > div.top-nav-panel-container > header > div > div > div.combo > div > div > div.el-select__selection > div.el-select__selected-item.el-select__placeholder > span.kling-new-tag');
+                if (element4) {
+                    element4.style.display = 'none';
+                };const element5 = document.querySelector('
+#app > div.customer-support');
+                if (element5) {
+                    element5.style.display = 'none';
+                };const element6 = document.querySelector('
+#app > div:nth-child(2) > header > div > div:nth-child(2) > div:nth-child(5)');
+                if (element6) {
+                    element6.style.display = 'none';
+                };const element7 = document.querySelector('
+#app > div:nth-child(2) > header > div > div:nth-child(2) > div:nth-child(6)');
+                if (element7) {
+                    element7.style.display = 'none';
+                };const element8 = document.querySelector('
+#app > div:nth-child(2) > header > div > div:nth-child(2) > div.membership');
+                if (element8) {
+                    element8.style.display = 'none';
+                };const element9 = document.querySelector('
+#app > div:nth-child(2) > header > div > div:nth-child(2) > div.menu-item.invitation-box > span');
+                if (element9) {
+                    element9.style.display = 'none';
+                };const element10 = document.querySelector('');
+                if (element10) {
+                    element10.style.display = 'none';
                 }
             }
         }
